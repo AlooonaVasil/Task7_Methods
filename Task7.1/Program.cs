63a69a33c74e3f1fa0c2fa25ed5229dee8bd295e
Алёна Алёна <alenchaeto1@outlook.com>
2022-10-18 11:23:11 +0300

    Добавьте файлы проекта.

---
+++ Task7.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа сравнивает площиди двух треугольников");
+            int a1 = GetSide(1, 1);
+            int b1 = GetSide(2, 1);
+            int c1 = GetSide(3, 1);
+            int a2 = GetSide(1, 2);
+            int b2 = GetSide(2, 2);
+            int c2 = GetSide(3, 2);
+
+            GetSquare(a1, b1, c1, out double s1);
+            GetSquare(a2, b2, c2, out double s2);
+            
+            if (s1 == 0 || s1 is double.NaN) Console.WriteLine("Треугольника 1 не существует");
+            else if (s2 == 0 || s2 is double.NaN) Console.WriteLine("Треугольника 2 не существует");
+            else if (s1 > s2)
+                Console.WriteLine("Площадь треугольника 1 ({0:F2}) больше площади треугольника 2 ({1:F2})", s1, s2);
+            else if (s1 < s2)
+                Console.WriteLine("Площадь треугольника 2 ({0:F2}) больше площади треугольника 1 ({1:F2})", s2, s1);
+            else
+                Console.WriteLine("Площадь треугольника 1 ({0:F2}) равна площади треугольника 2 ({1:F2})", s1, s2);
+            Console.ReadKey();
+        }
+        static int GetSide(int side, int trian)
+        {
+            Console.WriteLine("Введите {0} сторону треугольника {1}", side, trian);
+            int x = Convert.ToInt32(Console.ReadLine());
+            return x;
+        }
+        static double GetSquare(int a, int b, int c, out double s)
+        {
+            double p = (a + b + c) / 2.0;
+            s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+    }
+}
---
+++ Task7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа считает объем и площадь поверхности куба");
+            Console.WriteLine("Введите ребро куба");
+            double rebro = Convert.ToDouble(Console.ReadLine());
+            GetVolumeAndSquare(rebro, out double volume, out double square);
+            Console.WriteLine("Объем куба = {0:f2}", volume);
+            Console.WriteLine("Площадь поверхности куба = {0:f3}", square);
+            Console.ReadKey();
+        }
+        static void GetVolumeAndSquare(double rebro, out double volume, out double square)
+        {
+            volume = Math.Pow(rebro, 3);
+            square = 6 * Math.Pow(rebro, 2);            
+        }
+    }
+}
